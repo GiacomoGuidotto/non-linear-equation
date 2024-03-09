a207a99f0919cdc80b6f9686309971d836155b4c
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2024-03-09 22:23:20 +0100

    feat: ratios

--- fixed_secant.m
+++ fixed_secant.m
@@ -11,13 +11,32 @@ function [final_guess, guesses, interations, residual] = fixed_secant( ...
     k = interations;
 
     figure('Name','Fixed Secant method','NumberTitle','off');
-    subplot(2, 1, 1);
+    subplot(3, 2, 1);
     semilogy(1:k, guesses, '.-');
-    title("approximation of p_m");
+    title("approximation of $p_m$", 'interpreter', 'latex');
     xlabel("k"); ylabel("guess");
 
-    subplot(2, 1, 2);
+    subplot(3, 2, 3);
     semilogy(1:k, residual, '.-');
-    title("residual decay");
+    title("residual decay", 'interpreter', 'latex');
     xlabel("k"); ylabel("residual");
+
+    subplot(3, 2, 2);
+    ratio_1 = residual(2:end) ./ residual(1:end-1);
+    semilogy(2:k, ratio_1, '.-');
+    title('ratio 1: $\frac{d_k}{d_{k-1}}$', 'interpreter', 'latex');
+    xlabel("k"); ylabel("ratio");
+
+    subplot(3, 2, 4);
+    ratio_2 = residual(2:end) ./ (residual(1:end-1) .^ 2)
+    semilogy(2:k, ratio_2, '.-');
+    title('ratio 2: $\frac{d_k}{d_{k-1}^2}$', 'interpreter', 'latex');
+    xlabel("k"); ylabel("ratio");
+
+    subplot(3, 2, 6);
+    golden = (1 + sqrt(5)) / 2
+    ratio_3 = residual(2:end) ./ (residual(1:end-1) .^ golden)
+    semilogy(2:k, ratio_3, '.-');
+    title('ratio 3: $\frac{d_k}{d_{k-1}^{1.618}}$', 'interpreter', 'latex');
+    xlabel("k"); ylabel("ratio");
 end
