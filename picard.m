42c2f16c45950c651e2652e495766cbe8252d81c
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2024-03-14 11:49:46 +0100

    fix: init re

--- picard.m
+++ picard.m
@@ -5,6 +5,11 @@ function [zero, x, k, re] = picard( ...
     x = x0;
     re = Inf(1, length(x));
 
+    % init re
+    for k = 2:length(x)
+        re(k) = abs(x(k) - x(k - 1)) / x(k - 1);
+    end
+
     % Picard iteration
     k = length(x);
     while re(k) > tolerance && k < kmax
