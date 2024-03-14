8f02afc9f1818e79571f9cba1950f40e215e72c8
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2024-03-14 11:13:00 +0100

    refactor: ratio plots

--- fxsecant.m
+++ fxsecant.m
@@ -9,41 +9,40 @@ function zero = fxsecant( ...
     );
 
     ratio_1 = residual(2:end) ./ residual(1:end-1);
+    ratio_1 = [Inf ratio_1];
 
     ratio_2 = residual(2:end) ./ (residual(1:end-1) .^ 2);
+    ratio_2 = [Inf ratio_2];
 
     golden = (1 + sqrt(5)) / 2;
     ratio_3 = residual(2:end) ./ (residual(1:end-1) .^ golden);
+    ratio_3 = [Inf ratio_3];
 
     f = figure();
     f.Name = 'Fixed Secant method';
     f.NumberTitle = 'off';
-    f.Position = [800, 0, 800, 900];
+    f.Position = [500, 0, 500, 900];
 
-    subplot(3, 2, 1);
+    subplot(3, 1, 1);
     semilogy(x, '.-');
     title("approximation of $p_m$", 'interpreter', 'latex');
     xlabel("k"); ylabel("guess");
     yline(zero, ':', 'final guess');
 
-    subplot(3, 2, 3);
+    subplot(3, 1, 2);
     semilogy(residual, '.-');
     title("residual decay", 'interpreter', 'latex');
     xlabel("k"); ylabel("residual");
     yline(tolerance, ':', 'tolerance');
 
-    subplot(3, 2, 2);
-    semilogy(ratio_1, '.-');
-    title('ratio 1: $\frac{d_k}{d_{k-1}}$', 'interpreter', 'latex');
-    xlabel("k"); ylabel("ratio");
-
-    subplot(3, 2, 4);
-    semilogy(ratio_2, '.-');
-    title('ratio 2: $\frac{d_k}{d_{k-1}^2}$', 'interpreter', 'latex');
-    xlabel("k"); ylabel("ratio");
-
-    subplot(3, 2, 6);
-    semilogy(ratio_3, '.-');
-    title('ratio 3: $\frac{d_k}{d_{k-1}^{1.618}}$', 'interpreter', 'latex');
+    subplot(3, 1, 3);
+    semilogy(1:length(ratio_1), ratio_1, '.-', ...
+            1:length(ratio_2), ratio_2, '.-',...
+            1:length(ratio_3), ratio_3, '.-');
+    title('ratios convergence', 'interpreter', 'latex');
+    legend('$\frac{d_k}{d_{k-1}}$', ...
+            '$\frac{d_k}{d_{k-1}^2}$', ...
+            '$\frac{d_k}{d_{k-1}^{1.618}}$', ...
+             'interpreter', 'latex');
     xlabel("k"); ylabel("ratio");
 end
